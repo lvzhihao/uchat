e3d7bd7dbe9dba8e002c6bb63c89dc6fefce2804
edwin <edwin.lzh@gmail.com>
2018-01-03 19:46:13 +0800

    fix

--- model.go
+++ model.go
@@ -67,8 +67,9 @@ func ConvertUchatMessage(b []byte) ([]*UchatMessage, error) {
 	return ret, nil
 }
 
-// keyword msgpack
+// keyword model
 type UchatKeyword struct {
+	MerchantNo         string
 	LogSerialNo        string
 	ChatRoomSerialNo   string
 	FromWxUserSerialNo string
@@ -83,6 +84,10 @@ func ConvertUchatKeyword(b []byte) ([]*UchatKeyword, error) {
 	if err != nil {
 		return nil, err
 	}
+	merchantNo, ok := rst["vcMerchantNo"]
+	if !ok {
+		return nil, errors.New("empty merchantNo")
+	}
 	data, ok := rst["Data"]
 	if !ok {
 		return nil, errors.New("empty Data")
@@ -95,11 +100,67 @@ func ConvertUchatKeyword(b []byte) ([]*UchatKeyword, error) {
 	ret := make([]*UchatKeyword, 0)
 	for _, v := range list {
 		key := &UchatKeyword{}
+		key.MerchantNo = goutils.ToString(merchantNo)
 		key.LogSerialNo = goutils.ToString(v["vcSerialNo"])
 		key.ChatRoomSerialNo = goutils.ToString(v["vcChatRoomSerialNo"])
 		key.FromWxUserSerialNo = goutils.ToString(v["vcFromWxUserSerialNo"])
 		key.ToWxUserSerialNo = goutils.ToString(v["vcToWxUserSerialNo"])
 		key.Content = goutils.ToString(v["vcContent"])
+		ret = append(ret, key)
+	}
+	return ret, nil
+}
+
+// MemberJoin model
+type UchatMemberJoin struct {
+	MerchantNo           string
+	ChatRoomSerialNo     string
+	WxUserSerialNo       string
+	FatherWxUserSerialNo string
+	WxId                 string
+	NickName             string
+	HeadImages           string
+	JoinChatRoomType     int32
+	JoinDate             time.Time
+	ExtraData            interface{} //补充数据，并非接口返回
+}
+
+func ConvertUchatMemberJoin(b []byte) ([]*UchatMemberJoin, error) {
+	var rst map[string]interface{}
+	err := json.Unmarshal(b, &rst)
+	if err != nil {
+		return nil, err
+	}
+	merchantNo, ok := rst["vcMerchantNo"]
+	if !ok {
+		return nil, errors.New("empty merchantNo")
+	}
+	data, ok := rst["Data"]
+	if !ok {
+		return nil, errors.New("empty Data")
+	}
+	var list []map[string]interface{}
+	err = json.Unmarshal([]byte(goutils.ToString(data)), &list)
+	if err != nil {
+		return nil, err
+	}
+	ret := make([]*UchatMemberJoin, 0)
+	for _, v := range list {
+		key := &UchatMemberJoin{}
+		key.MerchantNo = goutils.ToString(merchantNo)
+		key.ChatRoomSerialNo = goutils.ToString(v["vcChatRoomSerialNo"])
+		key.WxUserSerialNo = goutils.ToString(v["vcWxUserSerialNo"])
+		key.FatherWxUserSerialNo = goutils.ToString(v["vcFatherWxUserSerialNo"])
+		key.WxId = goutils.ToString(v["vcWxId"])
+		nickName, err := base64.StdEncoding.DecodeString(goutils.ToString(v["vcBase64NickName"]))
+		if err != nil {
+			key.NickName = goutils.ToString(v["vcNickName"])
+		} else {
+			key.NickName = goutils.ToString(nickName)
+		}
+		key.HeadImages = goutils.ToString(v["vcHeadImages"])
+		key.JoinChatRoomType = goutils.ToInt32(v["nJoinChatRoomType"])
+		key.JoinDate, _ = time.ParseInLocation("2006-01-02T15:04:05.999", goutils.ToString(v["dtCreateDate"]), UchatTimeLocation)
 		ret = append(ret, key)
 	}
 	return ret, nil
